a785e9cb94dbeae22b7dc767f140bcb67e9d7751
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2022-09-29 20:34:49 -0300

    last commit

--- functional-components/src/App.js
+++ functional-components/src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
+import Movies from './movies/movies';
 import './App.css';
 
+const myData = [
+  { Framework: "Boostrap"} ,
+  { Framework: "Material"} ,
+  { Framework: "Tailwind"} ,
+  { Framework: "Next"}
+]
+//[ "Bootstrap", "Material", "Tailwind", "Next" ];
 
+/*function SumoDosNumero(a,b ){
+  return a+b;
+}*/
+
+// Repaso funcion anonima/arrow function
+/*const SumoDosNumero = (a,b) => {
+  return a+b;
+}*/
+
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
 function App() {
+
+  const filterResult = words.filter(word => word.length > 6);
+
+  const myDataList = myData.map( (element) => {
+      return ( <h2> { element.Framework } </h2> )
+  } );
+
+  /* <h2> { myData[0].Framework } </h2>
+      <h2> { myData[1].Framework } </h2>
+      <h2> { myData[2].Framework } </h2>
+      <h2> { myData[3].Framework } </h2>
+*/
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { myDataList }
+      <Movies data={ filterResult } ></Movies>
     </div>
   );
 }
