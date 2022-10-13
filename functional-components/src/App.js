8ddf71806155eaf90a2fbe7c38be7dbc039acd6c
José Luis Elustondo <joselus62@yahoo.com.ar>
2022-10-13 10:17:44 -0300

    Carrusel de Imagenes

--- functional-components/src/App.js
+++ functional-components/src/App.js
@@ -1,45 +1,106 @@
-import logo from './logo.svg';
-import image from "./assets/img/netflix.png";
-import './App.css';
-import {Sound} from './component/sound/Sound';
-import {Video} from './component/video/Video';
-import aud from './assets/audio/ANBR - Learn to Fly.mp3';
-import vid from './assets/video/VID-20211231-WA0118.mp4';
+import React, { Component } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const items = [
+  {
+    src: require ('./assets/img/Aconcagua.jpg'),
+    altText: 'Imagen1',
+    caption: 'Cerro Aconcagua'
+  },
+  {
+    src: require ('./assets/img/Monumento.jpg'),
+    altText: 'Imagen2',
+    caption: 'Monumento al Ejército de los Andes'
+  },
+  {
+    src: require ('./assets/img/Potrerillos.jpg'),
+    altText: 'Imagen3',
+    caption: 'Dique Potrerillos'
+  },
+  {
+    src: require ('./assets/img/ValleGrande.jpg'),
+    altText: 'Imagen4',
+    caption: 'Dique Valle Grande'
+  },
+  {
+    src: require ('./assets/img/Viñedo.jpg'),
+    altText: 'Imagen5',
+    caption: 'Viñedo Mendocino'
+  }
+];
 
-//import HelloWorld from './component/banner/Banner';
-import { Banner as Saludo } from './component/banner/Banner';
-//import './component/banner/Banner';//Traigo todo lo que tengo en Banner.js
-import Navbar from'./component/navbar/Navbar';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
 
+  onExiting() {
+    this.animating = true;
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Saludo msg="Hola Mundo!"/>
-      <Navbar src={image} size={40}/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-      <Sound src={aud} type='audio/mpeg' />
-        <br />
-      <Video src={vid} type='video/mp4' />
-        <br />
+  onExited() {
+    this.animating = false;
+  }
 
-        Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.src}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <img src={item.src} alt={item.altText} width="100%" />
+          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        </CarouselItem>
+      );
+    });
+
+    return (
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+      >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+      </Carousel>
+    );
+  }
 }
 
-//HelloWorld();
 
-export default App;
+export default App;                                                                      
